87a919cca80df9ed95e336a12e7257b805ad1cd3
Robin Luiten <robin.luiten@gmail.com>
2011-04-30 11:58:50 +1000

    Added test for TraverseTree including checking order of action.

--- src/cdeLibTest/CommonEntryTest.cs
+++ src/cdeLibTest/CommonEntryTest.cs
@@ -44,6 +44,35 @@ namespace cdeLibTest
             mockAction.AssertWasCalled(x => x("d2", de2));
         }
 
+        [Test]
+        public void TraverseTree_TreeHeirArchy_CallsActionOnAllChildren()
+        {
+            var de1 = new DirEntry { Name = "d1" }; // this just the root to work 'from'
+            var de2a = new DirEntry { Name = "d2a" };
+            var de2b = new DirEntry { Name = "d2b", IsDirectory = true };
+            var de2c = new DirEntry { Name = "d2c" };
+            de1.Children.Add(de2a);
+            de1.Children.Add(de2b);
+            de1.Children.Add(de2c);
+            var de3a = new DirEntry { Name = "d3a", IsDirectory = true };
+            de2b.Children.Add(de3a);
+            var de4a = new DirEntry { Name = "d4a" };
+            de3a.Children.Add(de4a);
+
+            var mockAction = MockRepository.GenerateMock<Action<string, DirEntry>>();
+            using (mockAction.GetMockRepository().Ordered())
+            {
+                mockAction.Expect(x => x("d2a", de2a)).Repeat.Times(1);
+                mockAction.Expect(x => x("d2b", de2b)).Repeat.Times(1);
+                mockAction.Expect(x => x("d2c", de2c)).Repeat.Times(1);
+                mockAction.Expect(x => x(@"d2b\d3a", de3a)).Repeat.Times(1);
+                mockAction.Expect(x => x(@"d2b\d3a\d4a", de4a)).Repeat.Times(1);
+            }
+
+            de1.TraverseTree("", mockAction);
+
+            mockAction.VerifyAllExpectations();
+        }
     }
     // ReSharper restore InconsistentNaming
 }
