731e083eb64e334b4490fb88df2f465448bb0bf1
Robin Luiten <robin.luiten+git@gmail.com>
2011-11-22 14:11:14 +1000

    Overhauled tree view just in time population.
    Code is simpler now.

--- src/cdWin/DisplayTreeFromRootPresenter.cs
+++ src/cdWin/DisplayTreeFromRootPresenter.cs
@@ -10,6 +10,7 @@ namespace cdeWin
 
     public class DisplayTreeFromRootPresenter : Presenter<IDisplayTreeFromRootForm>, IDisplayTreeFromRootPresenter
     {
+        private const string DummyNodeName = "_dummyNode";
         private readonly IDisplayTreeFromRootForm _clientDisplayTreeFromRootFormForm;
         private readonly RootEntry _rootEntry;
 
@@ -31,72 +32,71 @@ namespace cdeWin
             }
         }
 
-        /// <summary>
-        /// Load RootEntry and first level directories into TreeView.
-        /// </summary>
         public void LoadData()  // OnLoadData handler
         {
-            _clientDisplayTreeFromRootFormForm.TreeViewNodes = BuildTopLevelTreeNodes();
+            _clientDisplayTreeFromRootFormForm.TreeViewNodes = BuildRootNode();
         }
 
-        private TreeNode BuildTopLevelTreeNodes()
+        private TreeNode BuildRootNode()
         {
-            var rootTreeNode = new TreeNode(_rootEntry.RootPath)
-                                   {ImageIndex = 0, Tag = _rootEntry};
-            if (_rootEntry.Children != null)
-            {
-                foreach (var dirEntry in _rootEntry.Children)
-                {
-                    if (dirEntry.IsDirectory)
-                    {
-                        var entry = rootTreeNode.Nodes.Add(dirEntry.Name);
-                        //entry.ImageIndex = 0;
-                        entry.Tag = dirEntry;
-
-                        AddChildrenToTreeNode(entry, dirEntry);
-                    }
-                }
-            }
+            var rootTreeNode = NewTreeNode(_rootEntry, _rootEntry.RootPath);
+            SetDummyChildNode(rootTreeNode, _rootEntry);
             return rootTreeNode;
         }
 
-        private static void AddChildrenToTreeNode(TreeNode treeNode, CommonEntry dirEntry)
+        /// <summary>
+        /// A node with children gets a dummy child node to display as an expandable node.
+        /// </summary>
+        private static void SetDummyChildNode(TreeNode treeNode, CommonEntry commonEntry)
         {
-            foreach (var subDirEntry in dirEntry.Children)
+            if (commonEntry.Children.Any(entry => entry.IsDirectory))
             {
-                if (subDirEntry.IsDirectory)
-                {
-                    var subEntry = treeNode.Nodes.Add(subDirEntry.Name);
-                    //subEntry.ImageIndex = 0;
-                    subEntry.Tag = subDirEntry;
-                }
+                treeNode.Nodes.Add(NewTreeNode(null, DummyNodeName));
             }
         }
 
         public void BeforeExpandNode()  // OnBeforeExpandNode handler
         {
-            var expandNode = _clientDisplayTreeFromRootFormForm.ActiveBeforeExpandNode;
-            foreach (TreeNode treeNode in expandNode.Nodes)
-            {
-                foreach (TreeNode subTreeNode in treeNode.Nodes)
-                {
-                    var subDirEntry = (DirEntry)subTreeNode.Tag;
-                    if (subDirEntry.IsDirectory)
-                    {
-                        // if subTreeNode.Nodes allready has members its had children done.
-                        if (subTreeNode.Nodes.Count == 0 && subDirEntry.Children.Count > 0)
-                        {
-                            AddChildrenToTreeNode(subTreeNode, subDirEntry);
-                        }
-                    }
-                }
-            }
-
-            // conversely - when collapse remove nodes no longer required at more than 1 level away ?
-            // No for collapse just leave nodes that way expand puts back tree in previous expanded state.
+            CreateNodesPreExpand(_clientDisplayTreeFromRootFormForm.ActiveBeforeExpandNode);
         }
 
+        private static void CreateNodesPreExpand(TreeNode parentNode)
+        {
+            var n = parentNode.Nodes[0];
+            var b = n.Nodes;
+            if (parentNode.Nodes.Count == 1 && parentNode.Nodes[0].Text == DummyNodeName)
+            {
+                // Replace Dummy with real nodes now that will be visible.
+                parentNode.Nodes.Clear();
+                AddAllDirectoriesChildren(parentNode, (CommonEntry) parentNode.Tag);
+            }
+        }
 
+        private static void AddAllDirectoriesChildren(TreeNode treeNode, CommonEntry dirEntry)
+        {
+            foreach (var subDirEntry in dirEntry.Children)
+            {
+                AddDirectoryChildren(treeNode, subDirEntry);
+            }
+        }
+
+        private static void AddDirectoryChildren(TreeNode treeNode, DirEntry dirEntry)
+        {
+            if (dirEntry.IsDirectory)
+            {
+                var newTreeNode = NewTreeNode(dirEntry, dirEntry.Name);
+                treeNode.Nodes.Add(newTreeNode);
+                SetDummyChildNode(newTreeNode, dirEntry);
+            }
+        }
+
+        private static TreeNode NewTreeNode(object tag, string name)
+        {
+            return new TreeNode(name) {
+                //ImageIndex = 0, 
+                Tag = tag
+            };
+        }
     }
 
 }
