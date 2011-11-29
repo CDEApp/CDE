3b6feb4993a2486a209e066ad01ce4829b4b0ac2
Robin Luiten <robin.luiten+git@gmail.com>
2011-11-29 21:28:00 +1000

    Refactored test a bit.
    Figured out that a Stub should not be used with AssertWasCalled as it can just break as it did when adding a getter to a property.

--- src/cdeWinTest/TestDisplayTreeFromRootPresenter.cs
+++ src/cdeWinTest/TestDisplayTreeFromRootPresenter.cs
@@ -5,6 +5,9 @@ using Rhino.Mocks;
 using Rhino.Mocks.Constraints;
 using cdeLib;
 using cdeWin;
+//using Is = NUnit.Framework.Is;
+
+//using Is = NUnit.Framework.Is;
 
 namespace cdeWinTest
 {
@@ -12,41 +15,81 @@ namespace cdeWinTest
     [TestFixture]
     public class TestDisplayTreeFromRootPresenter
     {
-        [Test]
-        public void LoadData_action_displays_Tree_and_List()
+        const string testRootPath = @"C:\Testing";
+        const string testDirName = @"TestDir";
+        const string DummyNodeName = @"_dummyNode";
+
+        public static RootEntry CreateTestRoot()
         {
-            var mockView = MockRepository.GenerateStub<IDisplayTreeFromRootForm>();
-            const string testRootPath = @"C:\Testing";
-            const string testDirName = @"TestDir";
             var rootEntry = new RootEntry { RootPath = testRootPath };
             var dirEntry1 = new DirEntry
-                                {
-                                    IsDirectory = true,
-                                    Name = testDirName,
-                                    Modified = new DateTime(2011, 04, 14, 21, 09, 04, DateTimeKind.Local)
-                                };
+            {
+                IsDirectory = true,
+                Name = testDirName,
+                Modified = new DateTime(2011, 04, 14, 21, 09, 04, DateTimeKind.Local)
+            };
             var dirEntry2 = new DirEntry
-                                {
-                                    IsDirectory = false,
-                                    Name = "TestFile",
-                                    Size = 3311,
-                                    Modified = new DateTime(2011, 03, 13, 22, 10, 5, DateTimeKind.Local)
-                                };
+            {
+                IsDirectory = false,
+                Name = "TestFile",
+                Size = 3311,
+                Modified = new DateTime(2011, 03, 13, 22, 10, 5, DateTimeKind.Local)
+            };
             rootEntry.Children.Add(dirEntry1);
             rootEntry.Children.Add(dirEntry2);
             rootEntry.SetInMemoryFields();
+            return rootEntry;
+        }
 
-            var testPresenter = new DisplayTreeFromRootPresenter(mockView, rootEntry);
+        private IDisplayTreeFromRootForm _mockView;
+        private RootEntry _rootEntry;
+        private DisplayTreeFromRootPresenter _testPresenter;
 
-            testPresenter.LoadData();
+        [SetUp]
+        public void BeforeEveryTest()
+        {
+            _rootEntry = CreateTestRoot();
+            _mockView = MockRepository.GenerateMock<IDisplayTreeFromRootForm>();
+            _testPresenter = new DisplayTreeFromRootPresenter(_mockView, _rootEntry);
+        }
 
-            mockView.AssertWasCalled(x => x.TreeViewNodes 
-                = Arg<TreeNode>.Matches(
-                    new PredicateConstraint<TreeNode>(y =>
-                            y.Text == testRootPath
+        [Test]
+        public void LoadData_TreeViewNodes_IsSetForRoot()
+        {
+            _testPresenter.LoadData();
+
+            // now that have get; can do it this way instead of AssertWasCalled complexity.
+            //TreeNode rootNode = _mockView.TreeViewNodes;
+            //Assert.That(rootNode.Text, Is.EqualTo(testRootPath));
+            //Assert.That(rootNode.Nodes.Count, Is.EqualTo(1));
+            //Assert.That(rootNode.Nodes[0].Text, Is.EqualTo(DummyNodeName));
+
+            // this was working -- adding a getter to property breaks this 
+            // BROKE cause it was Stub not Mock - odd it breaks only once add getter though.
+            _mockView.AssertWasCalled(x => x.TreeViewNodes
+                = Arg<TreeNode>.Matches( new PredicateConstraint<TreeNode>(
+                        y => y.Text == testRootPath
                              && y.Nodes.Count == 1
-                             && y.Nodes[0].Text == testDirName
-                        )));
+                             && y.Nodes[0].Text == DummyNodeName )));
+        }
+
+        [Test]
+        public void ExpandRootNode_PoplulatesFirstLevelChildren()
+        {
+            _testPresenter.LoadData();
+            var rootNode = _mockView.TreeViewNodes;
+            _mockView.ActiveBeforeExpandNode = rootNode;
+
+            _testPresenter.BeforeExpandNode();
+
+         //   Assert.That(rootNode.Nodes.Count, Is.EqualTo(1));
+         //   Assert.That(rootNode.Nodes[0].Text, Is.EqualTo(testDirName));
+            _mockView.AssertWasCalled(x => x.TreeViewNodes
+                = Arg<TreeNode>.Matches(new PredicateConstraint<TreeNode>(
+                            y => y.Text == testRootPath
+                             && y.Nodes.Count == 1
+                             && y.Nodes[0].Text == testDirName)));
+
         }
 
         //public class PropertiesMatchConstraint : AbstractConstraint
