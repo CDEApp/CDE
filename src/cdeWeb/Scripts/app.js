51a8e722e5aa6b3ac8cb07fdaea19e8f439ae2e2
Robin Luiten <robin.luiten+git@gmail.com>
2013-06-22 17:50:43 +1000

    Grok'd more bootstrap... looks better now, widths better, layed out about better.
    Added bootstrap-responsive for a much nicer result when shrink width display.
    Added lodash.js [for restangular]
    Removed the different modules for now from angular app, worry about it later.
    Got routing to search to work and render.
    Switched to navbar-static-top for navbar so dont need to muck with padding-top for body.
    Fixed so that search button is only 'submit' button - means the clear results wont activated from enter now.
    directive restoresearchfocus refocuses search input if it was in focus when search launched [by enter].
    added "data-" prefixes to attributes to shut up warnings. adde '=""' to attributes for same reason.

--- src/cdeWeb/Scripts/app.js
+++ src/cdeWeb/Scripts/app.js
@@ -1,38 +1,35 @@
 ﻿'use strict';
 // ReSharper disable InconsistentNaming
 
-angular.module('cdeweb.service', ['restangular']);
-
-angular.module('cdeweb.directive', []);
-
-angular.module('cdeweb.filter', []);
-
 var cdeWebApp = angular.module('cdeweb', [
     'ngResource',
-    'cdeweb.service',
-    'cdeweb.directive',
-    'cdeweb.filter',
-    'ui.directives'
+    'ngCookies',
+    'ui.directives',
+    'restangular'
 ]);
 
 cdeWebApp.config(function($routeProvider) {
-    $routeProvider.
-        when('/', {
-            controller: cdeWebCtrl,
+    $routeProvider
+        .when('/about', {
+            controller: 'cdeWebAboutCtrl',
+            templateUrl: 'partials/about.html',
+            header: 'partials/navbar.html'
+            // hack ensure it grabs a new one when loaded, 
+            // todo consider a version tag appended for when version bumps ?
+            // and just make sure expire app.js, or version.js hourly... ?
+            //header: 'partials/navbar.html?r='+Math.random()
+        })
+        .when('/search/:query', {
+            controller: 'cdeWebCtrl',
             templateUrl: 'partials/cdeWeb.html',
             header: 'partials/navbar.html'
-        }).
-        when('/search/:query', {
-            controller: cdeWebSearchCtrl,
-            templateUrl: 'partials/cdeWebSearch.html',
-            header: 'partials/navbar.html'
-        }).
-        when('/copyPath/:path', {
-            controller: cdeWebCopyCtrl,
+        })
+        .when('/copyPath/:path', {
+            controller: 'cdeWebCopyCtrl',
             templateUrl: 'partials/copyPath.html',
             header: 'partials/navbar.html'
-        }).
-        otherwise({ redirectTo: '/' });
+        })
+        .otherwise({ redirectTo: '/about' });
 });
 
 cdeWebApp.config(function (RestangularProvider) {
@@ -40,7 +37,12 @@ cdeWebApp.config(function (RestangularProvider) {
     r.setBaseUrl('/odata');
     r.setListTypeIsArray(false); // odata returns an object with a value field for list.
     
-    // Not sure want to muck with response object format...
+    // Not modifying the response format for now. but this is how it culd be modified
+    // NOTE: possibly should make a modified response which returns an object
+    //       with 3 fields as below, possibly should make each field a promise
+    //       so fields can be assigned immediately rather than waiting on promise resolve.
+    //
+    
     //r.setResponseExtractor(function(response, operation, what, url) {
     //    if (operation === 'getList') {
     //        // rearrange odata response.
@@ -70,17 +72,36 @@ cdeWebApp.directive('selectall', function () {
     };
 });
 
-cdeWebApp.run(function($rootScope, $route, $location) {
-        $rootScope.layoutPartial = function (partialName) {
-            //console.log('$route.current', $route.current);
-            //console.log('partialName', partialName);
-            //console.log('$route.current[partialName]', $route.current[partialName]);
+cdeWebApp.run(function($rootScope, $route) {
+    $rootScope.layoutPartial = function (partialName) {
+        //console.log('$route.current[partialName]', $route.current[partialName]);
+        return $route.current[partialName];
+    };
 
-            return $route.current[partialName];
-        };
+    // $routeChangeStart
+    // $routeChangeSuccess
+
+    $rootScope.$on('$routeChangeSuccess', function(next, current) {
+        // if route changed ? cause a search - this is to cause a search on route load to happen ?
     });
+});
 
-var cdeWebCopyCtrl = function($scope, $location, $routeParams) {
+
+//cdeWebApp.run(function ($rootScope, $templateCache) {
+//    //// helps on not caching partials, not sure why as browser is caching not js code.
+//    //// but this runs on ever time view content is loaded - far to often.
+//    //$rootScope.$on('$viewContentLoaded', function () {
+//    //    // this addresses cached partials not refreshing with edits..
+//    //    console.log('ker-flush!');
+//    //    $templateCache.removeAll();
+//    //});
+//    
+//    //$templateCache.removeAll();  // see if just on app load it does its thing.. doesnt work
+//});
+
+var controllers = {};
+
+controllers.cdeWebCopyCtrl = function($scope, $location, $routeParams) {
     $scope.resultPath = $routeParams.path;
     console.log('$location', $location);
     console.log('$routeParams.path', $routeParams.path);
@@ -88,75 +109,13 @@ var cdeWebCopyCtrl = function($scope, $location, $routeParams) {
     //after copy leave ? //$location.path('/#');
 };
 
-var cdeWebSearchCtrl = function ($scope, $routeParams, $location, version) {
-    console.log('cdeWebSearchCtrl setup', $routeParams.query);
-    $scope.data.query = $routeParams.query;
-    $scope.data.searchResult = searchResultTest;
+controllers.navbarCtrl = function ($scope, $location, DataModel, DirEntryRepository) {
+    console.log('navbarCtrl init');
+    $scope.data = DataModel;
 
-    $scope.clearResult = function () {
-        $scope.data.searchResult = [];
-    };
-    
-    $scope.clearQuery = function () {
-        $scope.data.query = '';
-    };
-
-    $scope.search = function () {
-        console.log('search [' + $scope.data.query + ']');
-        //$location.path('/search/' + $scope.data.query);
-
-        $scope.data.searchResult = searchResultTest;
-    };
-    
-    $scope.showClearResult = function () { // TODO modify to go back to no path.
-        return $scope.data.searchResult.length !== 0;
-    };
-};
-
-var cdeWebCtrl = function ($scope, $location, version, DirEntryRepository) {
-    $scope.clearResult = function() {
-        $scope.data.searchResult = [];
-    };
-
-    $scope.clearQuery = function() {
-        $scope.data.query = '';
-    };
-
-    $scope.search = function () {
-        console.log('search query', $scope.data.query);
-        DirEntryRepository.get($scope.data.query)
-            .then(function (blah) {
-                console.log('nextLink', blah['odata.nextLink']);
-                $scope.data.metadata = blah['odata.metadata'];
-                $scope.data.nextLink = blah['odata.nextLink'];
-                $scope.data.searchResult = blah.value;
-            });
-    };
-
-    $scope.hideClear = function() {
-        return !$scope.data.query;
-    };
-
-    $scope.showClearResult = function() {
-        return $scope.data.searchResult.length !== 0;
-    };
-
-    $scope.copyPathDialog = function(path) {
-        //var s = $scope;
-        //var rs = $scope.resultPath;
-        //var p = path;
-        console.log('path ' + path);
-        $('#copyPathDialog').modal({});
-    };
-
-    // make our reset on input field only visible when query has value.
-    // - how do i make this part of the reset control ?
-    // - be nice if it didnt need to know model 'name' or control to fiddle class on ?
-    // - reset creates the anchor so should be avail ?
-    // - reset require ng-model we should be able to get its value ?
-    // - then just create an extra watch hmm :) :) :)
 
     // modify ui-reset to only have remove icon visible if input has content.
+    // This logic could be pushed back into uiReset possibly ? makes for easier styling control ?
     $scope.$watch('data.query', function queryWatch() {
         if ($scope.model === null || $scope.data.query === null || $scope.data.query === "") {
             $('a.hascontent').removeClass('hascontent');
@@ -165,39 +124,98 @@ var cdeWebCtrl = function ($scope, $location, version, DirEntryRepository) {
         }
         //console.log("$scope.data.query:" + $scope.data.query);
     });
+    
+    $scope.clearResult = function () {
+        $scope.data.searchResult = [];
+    };
 
-    // TODO make Escape key in input - clear it... or maybe two escapes clears it ?
+    $scope.clearQuery = function () {
+        $scope.data.query = '';
+    };
 
-    var data = {};
-    $scope.data = data;
-    data.query = '';
-    data.version = version;
-    data.name = 'cdeWeb';
-    data.queryOptions = {};
-    $scope.clearQuery();
-    $scope.clearResult();
+    $scope.searchInputActive = function() {
+        return document.activeElement.id === 'search';
+    };
+    
+    $scope.search = function () {
+        console.log('navbarCtrl.search [' + $scope.data.query + ']');
+        //console.log(document.activeElement, document.activeElement.id);
+        $scope.data.searchInputActive = $scope.searchInputActive();
+        $scope.clearResult();
+        $location.path('/search/' + $scope.data.query);
+
+        DirEntryRepository.get($scope.data.query)
+            .then(function (blah) {
+                $scope.data.metadata = blah['odata.metadata'];
+                $scope.data.nextLink = blah['odata.nextLink'];
+                $scope.data.searchResult = blah.value;
+                //console.log('nextLink', $scope.data.nextLink);
+                //console.log('value', $scope.data.searchResult.length);
+            });
+    };
+    
+    $scope.haveResults = function () {
+        return $scope.data.searchResult.length !== 0;
+    };
 };
 
-var searchResultTest = [
-    {Name: 'moo1', Size: 10, Modified: 'adate1', Path: 'C:\\Here'},
-    {Name: 'moo2', Size: 12, Modified: 'adate2', Path: 'C:\\'},
-    {Name: 'Here', Size: 10, Modified: 'adate3', Path: 'C:\\'},
-    {Name: 'Deep1', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate'},
-    {Name: 'Deep2 Six', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate'},
-    {Name: 'Deep3 Six', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate'},
-    {Name: 'Deep4 Six', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate'}
-];
-// pull Contact from web.config - so roy can setup to him ?
+cdeWebApp.controller(controllers);
 
-angular.module('cdeweb.service')
-    .value('version', '0.1')
-    .service('DirEntryRepository', function (Restangular) {
-        return {
-            get: function (substring) {
-                var baseDE = Restangular.all("DirEntries" + substringFilter(substring, 'Name'));
-                return baseDE.getList();
-            }
+cdeWebApp.controller('cdeWebAboutCtrl', function ($rootScope, $scope, $location, DataModel) {
+    console.log('cdeWebAboutCtrl init');
+    $scope.data = DataModel;
+
+});
+
+// todo make Escape key in input query - clear it... or maybe two escapes clears it ?
+cdeWebApp.controller('cdeWebCtrl', function ($scope, $location, DataModel) {
+    console.log('cdeWebCtrl init');
+    $scope.data = DataModel;
+    console.log('$scope.data.query', $scope.data.query);
+    console.log('$scope.data.searchResult.length', $scope.data.searchResult.length);
+    //$scope.search();
+    
+    $scope.haveResults = function () {
+        return $scope.data.searchResult.length !== 0;
+    };
+    
+    // use web browser modal dialog for clipboard copy hackery. ABONDONED at moment.
+    $scope.copyPathDialog = function (path) {
+        console.log('path ' + path);
+        $('#copyPathDialog').modal({});
+    };
+});
+
+cdeWebApp.directive('restoresearchfocus', function() {
+    return function (scope, element) {
+        if (scope.data.searchInputActive && !scope.searchInputActive()) {
+            element[0].focus();
+            scope.data.searchInputActive = false;
+        }
+    };
+});
+
+cdeWebApp.service('DirEntryRepository', function (Restangular) {
+        this.get = function(substring) {
+            console.log('DirEntryRepository.get()');
+            var baseDE = Restangular.all("DirEntries" + substringFilter(substring, 'Name'));
+            return baseDE.getList();
         };
+    })
+    .factory('DataModel', function($rootScope, $cookies) {
+        var data = $rootScope.data;;
+        if (!data) {
+            console.log('DataModel new');
+            data = {
+                email: 'rob@queenofblad.es',
+                name: 'cdeWeb',
+                version: '0.1',
+                query: '',
+                queryOptions: {},
+                searchResult: []
+            };
+        }
+        return data;
     });
 
 function substringFilter(query, field) {
@@ -205,6 +223,17 @@ function substringFilter(query, field) {
     return "?$filter=substringof('" + notNullQuery + "'," + field + ")";
 }
 
+
+var searchResultTest = [
+    { Name: 'moo1', Size: 10, Modified: 'adate1', Path: 'C:\\Here' },
+    { Name: 'moo2', Size: 12, Modified: 'adate2', Path: 'C:\\' },
+    { Name: 'Here', Size: 10, Modified: 'adate3', Path: 'C:\\' },
+    { Name: 'Deep1', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate' },
+    { Name: 'Deep2 Six', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate' },
+    { Name: 'Deep3 Six', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate' },
+    { Name: 'Deep4 Six', Size: 13, Modified: 'adate3', Path: 'C:\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate\\BigLong\\Stuff\\With\\A\\Longis\\Long\\Path\\To\\Demonstrate' }
+];
+// pull Contact from web.config - so roy can setup to him ?
 // ReSharper restore InconsistentNaming
 
 
