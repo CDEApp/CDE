2be206e61cc8b48ebdb2cb69f5a2ed0d9224ae22
Robin Luiten <robin.luiten+git@gmail.com>
2013-07-04 09:29:58 +1000

    Removing SignalR sample from project.
    Updated NSubstitute.

--- src/cdeWeb/App_Start/RegisterHubs.cs
+++ src/cdeWeb/App_Start/RegisterHubs.cs
@@ -2,17 +2,14 @@ using System.Web.Routing;
 using Microsoft.AspNet.SignalR;
 using cdeWeb.App_Start;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(RegisterHubs), "Start")]
-
 namespace cdeWeb.App_Start
 {
     public static class RegisterHubs
     {
-        public static void Start()
+        public static void Start(RouteCollection routes)
         {
             // Register the default hubs route: ~/signalr
-            //RouteTable.Routes.MapHubs();
-            RouteTable.Routes.MapHubs(new HubConfiguration() { EnableJavaScriptProxies = false });
+            routes.MapHubs(new HubConfiguration() { EnableJavaScriptProxies = false });
         }
     }
 }
