f04841b21e6cb04b78763704e5f03e0dcbfd4e03
Robin Luiten <robin.luiten+git@gmail.com>
2014-07-06 12:13:07 +1000

    Updated to latest karma for execution of test of signalrAngular.js service module.

--- src/tests/karma.conf.js
+++ src/tests/karma.conf.js
@@ -1,56 +1,71 @@
 // Karma configuration
-// Generated on Tue Jul 02 2013 10:20:21 GMT+1000 (E. Australia Standard Time)
+// Generated on Sun Jul 06 2014 12:07:51 GMT+1000 (E. Australia Standard Time)
 
-// base path, that will be used to resolve files and exclude
-basePath = '../cdeWeb';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'Scripts/jquery-2.0.3.js',
-  'Scripts/angular.js',
-  'Scripts/angular-mocks.js',
-  'Scripts/signalrAngular.js',
-  '../tests/signalrAngular_tests.js'
-];
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '../cdeWeb',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-// web server port
-port = 9876;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+      'Scripts/jquery-2.0.3.js',
+      'Scripts/angular.js',
+      'Scripts/angular-mocks.js',
+      'Scripts/signalrAngular.js',
+      '../tests/signalrAngular_tests.js'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['ChromeCanary'];
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    
+    },
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
