c2128f4622027eb4f75ddcb7f489db0190cdf471
Jason Finch <jason.finch@gmail.com>
2020-04-08 16:25:02 +1000

    chore (Migration) Remove code we don't need for migration

--- src/cdeDataStructurev3/Entities/CommonEntry.cs
+++ src/cdeDataStructurev3/Entities/CommonEntry.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ProtoBuf;
 
@@ -8,10 +9,12 @@ namespace cdeDataStructure3.Entities
     /// Returns true if want traversal to continue after this returns.
     /// </summary>
     public delegate bool TraverseFunc(CommonEntry ce, DirEntry de);
+    
+    public delegate bool TraverseFuncWithRoot(CommonEntry ce, DirEntry de, RootEntry rootEntry = null);
 
     [ProtoContract
-     , ProtoInclude(1, typeof(RootEntry))
-     , ProtoInclude(2, typeof(DirEntry))]
+    ,ProtoInclude(1, typeof(RootEntry))
+    ,ProtoInclude(2, typeof(DirEntry))]
     public abstract class CommonEntry
     {
         public RootEntry RootEntry { get; set; }
@@ -44,10 +47,10 @@ namespace cdeDataStructure3.Entities
         /// </summary>
         public bool PathProblem;
 
-
+        
         public void TraverseTreePair(TraverseFunc func)
         {
-            TraverseTreePair(new List<CommonEntry> {this}, func);
+            TraverseTreePair(new List<CommonEntry> { this }, func);
         }
 
         /// <summary>
@@ -56,25 +59,17 @@ namespace cdeDataStructure3.Entities
         /// <param name="rootEntries">Entries to traverse</param>
         /// <param name="traverseFunc">TraversalFunc</param>
         /// <param name="catalogRootEntry">Catalog root entry, show we can bind the catalog name to each entry</param>
-        public static void TraverseTreePair(IEnumerable<CommonEntry> rootEntries, TraverseFunc traverseFunc,
-            RootEntry catalogRootEntry = null)
+        public static void TraverseTreePair(IEnumerable<CommonEntry> rootEntries, TraverseFunc traverseFunc, RootEntry catalogRootEntry = null)
         {
-            if (traverseFunc == null)
-            {
-                return;
-            } // nothing to do.
+            if (traverseFunc == null) { return; } // nothing to do.
 
             var funcContinue = true;
-            var dirs = new Stack<CommonEntry>(rootEntries
-                .Reverse()); // Reverse to keep same traversal order as prior code.
+            var dirs = new Stack<CommonEntry>(rootEntries.Reverse()); // Reverse to keep same traversal order as prior code.
 
             while (funcContinue && dirs.Count > 0)
             {
                 var commonEntry = dirs.Pop();
-                if (commonEntry.Children == null)
-                {
-                    continue;
-                } // empty directories may not have Children initialized.
+                if (commonEntry.Children == null) { continue; } // empty directories may not have Children initialized.
 
                 foreach (var dirEntry in commonEntry.Children)
                 {
@@ -82,7 +77,6 @@ namespace cdeDataStructure3.Entities
                     {
                         commonEntry.RootEntry = catalogRootEntry;
                     }
-
                     funcContinue = traverseFunc(commonEntry, dirEntry);
                     if (!funcContinue)
                     {
@@ -92,6 +86,86 @@ namespace cdeDataStructure3.Entities
                     if (dirEntry.IsDirectory)
                     {
                         dirs.Push(dirEntry);
+                    }
+                }
+            }
+        }
+
+        public void TraverseTreesCopyHash(CommonEntry destination)
+        {
+            var dirs = new Stack<Tuple<string, CommonEntry, CommonEntry>>();
+            var source = this;
+
+            if (source == null || destination == null)
+            {
+                throw new ArgumentException("source and destination must be not null.");
+            }
+
+            var sourcePath = source.Path;
+            var destinationPath = destination.Path;
+
+            //if (sourcePath != destinationPath)
+            if (string.Compare(sourcePath, destinationPath, true) != 0)
+            {
+                throw new ArgumentException("source and destination must have same root path.");
+            }
+
+            // traverse every source entry copy across the meta data that matches on destination entry
+            // if it adds value to destination.
+            // if destination is not there source not processed.
+            dirs.Push(Tuple.Create(sourcePath, source, destination));
+
+            while (dirs.Count > 0)
+            {
+                var t = dirs.Pop();
+                var workPath = t.Item1;
+                var baseSourceEntry = t.Item2;
+                var baseDestinationEntry = t.Item3;
+
+                if (baseSourceEntry.Children != null)
+                {
+                    foreach (var sourceDirEntry in baseSourceEntry.Children)
+                    {
+                        var fullPath = System.IO.Path.Combine(workPath, sourceDirEntry.Path);
+
+                        // find if theres a destination entry available.
+                        // size of dir is irrelevant. date of dir we don't care about.
+                        var sourceEntry = sourceDirEntry;
+                        var destinationDirEntry = baseDestinationEntry.Children
+                            .FirstOrDefault(x => (x.Path == sourceEntry.Path));
+
+                        if (destinationDirEntry == null)
+                        {
+                            continue;
+                        }
+
+                        if (!sourceDirEntry.IsDirectory
+                            && sourceDirEntry.Modified == destinationDirEntry.Modified
+                            && sourceDirEntry.Size == destinationDirEntry.Size)
+                        {
+                            // copy MD5 if none in destination.
+                            // copy MD5 as upgrade to full if dest currently partial.
+                            if ((sourceDirEntry.IsHashDone)
+                                && (!destinationDirEntry.IsHashDone)
+                                ||
+                                ((sourceDirEntry.IsHashDone)
+                                 && (destinationDirEntry.IsHashDone)
+                                 && !sourceDirEntry.IsPartialHash
+                                 && destinationDirEntry.IsPartialHash
+                                ))
+                            {
+                                destinationDirEntry.IsPartialHash = sourceDirEntry.IsPartialHash;
+                                destinationDirEntry.Hash = sourceDirEntry.Hash;
+                            }
+                        }
+                        else
+                        {
+                            if (destinationDirEntry.IsDirectory)
+                            {
+                                dirs.Push(Tuple.Create(fullPath, (CommonEntry) sourceDirEntry,
+                                                       (CommonEntry) destinationDirEntry));
+                            }
+                        }
                     }
                 }
             }
@@ -109,6 +183,41 @@ namespace cdeDataStructure3.Entities
             return System.IO.Path.Combine(a, b);
         }
 
+        public static IEnumerable<DirEntry> GetDirEntries(RootEntry rootEntry)
+        {
+            return new DirEntryEnumerator(rootEntry);
+        }
+
+        public static IEnumerable<DirEntry> GetDirEntries(IEnumerable<RootEntry> rootEntries)
+        {
+            return new DirEntryEnumerator(rootEntries);
+        }
+
+        public static IEnumerable<PairDirEntry> GetPairDirEntries(RootEntry rootEntry)
+        {
+            return new PairDirEntryEnumerator(rootEntry);
+        }
+
+        public static IEnumerable<PairDirEntry> GetPairDirEntries(IEnumerable<RootEntry> rootEntries)
+        {
+            return new PairDirEntryEnumerator(rootEntries);
+        }
+
+        /// <summary>
+        /// Return List of CommonEntry, first is RootEntry, rest are DirEntry that lead to this.
+        /// </summary>
+        public List<CommonEntry> GetListFromRoot()
+        {
+            var activatedDirEntryList = new List<CommonEntry>(8);
+            for (var entry = this; entry != null; entry = entry.ParentCommonEntry)
+            {
+                activatedDirEntryList.Add(entry);
+            }
+            activatedDirEntryList.Reverse(); // list now from root to this.
+            return activatedDirEntryList;
+        }
+
+
         /// <returns>False if Null or Empty, True if entry name ends with Space or Period which is a problem on windows file systems.</returns>
         public bool IsBadPath()
         {
@@ -118,3 +227,5 @@ namespace cdeDataStructure3.Entities
         }
     }
 }
+
+
