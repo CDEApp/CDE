97b0d7458acca12fb21fb77963c5eb3e27448373
jafin <jason.finch@gmail.com>
2021-04-05 09:01:41 +1000

    tidy (Unused): Remove unused function.

--- src/cdeWin/StringExtension.cs
+++ src/cdeWin/StringExtension.cs
@@ -4,18 +4,6 @@ namespace cdeWin
 {
     public static class StringExtension
     {
-        public static string ToHRString(this ulong val)
-        {
-            if (val == 0)
-            {
-                return "0";
-            }
-            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB" };
-            var place = Convert.ToInt32(Math.Floor(Math.Log(val, 1024)));
-            var num = Math.Round(val / Math.Pow(1024, place), 1);
-            return num + " " + suf[place];
-        }
-
         public static string ToHRString(this long val)
         {
             if (val == 0)
