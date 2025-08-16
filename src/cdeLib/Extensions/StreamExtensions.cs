362f1f89891920d77c79b8c5fa317bb2fa05f0ec
Jason Finch <jason.finch@gmail.com>
2025-08-16 11:27:28 +1000

    refactor (Stream): Guard against null.

--- src/cdeLib/Extensions/StreamExtensions.cs
+++ src/cdeLib/Extensions/StreamExtensions.cs
@@ -7,9 +7,21 @@ public static class StreamExtensions
 {
     public static byte[] ToByteArray(this Stream stream)
     {
-        var length = stream.Length > int.MaxValue ? int.MaxValue : Convert.ToInt32(stream.Length);
-        var buffer = new byte[length];
-        stream.ReadExactly(buffer, 0, length);
-        return buffer;
+        if (stream == null) throw new ArgumentNullException(nameof(stream));
+    
+        var originalPosition = stream.Position;
+        try 
+        {
+            stream.Position = 0;
+            var length = stream.Length > int.MaxValue ? int.MaxValue : Convert.ToInt32(stream.Length);
+            var buffer = new byte[length];
+            stream.ReadExactly(buffer, 0, length);
+            return buffer;
+        }
+        finally 
+        {
+            stream.Position = originalPosition;
+        }
     }
+
 }
