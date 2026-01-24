7298c8371a9d44d9c82876ac57ba749f5e45509d
Jason Finch <jason.finch@gmail.com>
2026-01-24 23:07:21 +1000

    fix (cde/startup): Warn user when appsettings.json is missing. Fixes #5

--- src/cde/Config/ConfigBuilder.cs
+++ src/cde/Config/ConfigBuilder.cs
@@ -5,11 +5,19 @@ namespace cde.Config;
 
 public class ConfigBuilder
 {
+    public const string AppSettingsFileName = "appsettings.json";
+
+    public static bool AppSettingsExists()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), AppSettingsFileName);
+        return File.Exists(path);
+    }
+
     public IConfigurationRoot Build(string[] args)
     {
         return new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())   // required for single file application or it goes hunting in temp folder when it extracts.
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(AppSettingsFileName, optional: true, reloadOnChange: true)
             .AddCommandLine(args)
             .Build();
     }
